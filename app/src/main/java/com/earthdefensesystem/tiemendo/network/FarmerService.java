88d2de0e46c6cbe4e9087c89d96709512c11bce1
JTyzz <jtyzzer@gmail.com>
2019-06-11 00:40:04 -0700

    Added service class for Farmer views

--- app/src/main/java/com/earthdefensesystem/tiemendo/network/FarmerService.java
+++ app/src/main/java/com/earthdefensesystem/tiemendo/network/FarmerService.java
@@ -1,4 +1,22 @@
 package com.earthdefensesystem.tiemendo.network;
 
-public class FarmerService {
+import com.earthdefensesystem.tiemendo.model.Farmer;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface FarmerService {
+
+    @FormUrlEncoded
+    @POST("createuser")
+    Call<Farmer> createFarmer(
+
+    );
+
+    @FormUrlEncoded
+    @GET("allfarmers")
+    Call<Farmer> getFarmers();
 }
