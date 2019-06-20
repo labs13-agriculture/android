c8db495e41c562b661cd49a365261c8d7b2e04c4
JTyzz <jtyzzer@gmail.com>
2019-06-20 01:23:48 -0700

    Added endpoints for User and Itemtype.

--- app/src/main/java/com/earthdefensesystem/tiemendo/network/TiemeService.java
+++ app/src/main/java/com/earthdefensesystem/tiemendo/network/TiemeService.java
@@ -3,8 +3,10 @@ package com.earthdefensesystem.tiemendo.network;
 import com.earthdefensesystem.tiemendo.FarmerSearchActivity;
 import com.earthdefensesystem.tiemendo.model.Client;
 import com.earthdefensesystem.tiemendo.model.Installment;
+import com.earthdefensesystem.tiemendo.model.ItemType;
 import com.earthdefensesystem.tiemendo.model.Organization;
 import com.earthdefensesystem.tiemendo.model.Token;
+import com.earthdefensesystem.tiemendo.model.User;
 
 import java.util.List;
 
@@ -61,6 +63,41 @@ public interface TiemeService {
     Call<Client> deleteFarmerWithId(@Path("id") String id,
                                     @Header("Authorization: Bearer ") String accessToken);
 
+    @POST("/retailer/new")
+    Call<Client> addRetailer(@Query("address") String address, @Query("community") String community,
+                             @Query("dateofbirth") String dateofbirth, @Query("district") String district,
+                             @Query("educationlevel") String educationlevel, @Query("email") String email,
+                             @Query("firstName") String firstName, @Query("gender") String gender,
+                             @Query("landmark") String landmark, @Query("lead") Boolean lead,
+                             @Query("nationality") String nationality, @Query("phone") String phone,
+                             @Query("position") String position, @Query("region") String region,
+                             @Query("secondName") String secondName, @Query("startyear") int startyear,
+                             @Query("title") String title, @Query("type") String type);
+
+    @GET("/retailer/retailers")
+    Call<List<Client>> getRetailers(@Header("Authorization: Bearer ") String accessToken);
+
+    @GET("/retailer/{id}")
+    Call<Client> getRetailerWithId(@Path("id") String id,
+                                   @Header("Authorization: Bearer ") String accessToken);
+
+    @PUT("/retailer/update/{id}")
+    Call<Client> updateClientWithId(@Path("id") String id,
+                                    @Header("Authorization: Bearer ") String accessToken,
+                                    @Query("address") String address, @Query("community") String community,
+                                    @Query("dateofbirth") String dateofbirth, @Query("district") String district,
+                                    @Query("educationlevel") String educationlevel, @Query("email") String email,
+                                    @Query("firstName") String firstName, @Query("gender") String gender,
+                                    @Query("landmark") String landmark, @Query("lead") Boolean lead,
+                                    @Query("nationality") String nationality, @Query("phone") String phone,
+                                    @Query("position") String position, @Query("region") String region,
+                                    @Query("secondName") String secondName, @Query("startyear") int startyear,
+                                    @Query("title") String title, @Query("type") String type);
+
+    @DELETE("/retailer/delete/{id}")
+    Call<Client> deleteRetailerWithId(@Path("id") String id,
+                                      @Header("Authorization: Bearer ") String accessToken);
+
     @GET("/installments/{installmentId}")
     Call<Installment> getInstallmentById(@Path("installmentId") String id,
                                          @Header("Authorization: Bearer ") String accessToken);
@@ -77,4 +114,23 @@ public interface TiemeService {
                                                @Query("datepaid") String datepaid,
                                                @Query("mode") String mode,
                                                @Query("officer") String officer);
+
+    @POST("/itemtype/add")
+    Call<ItemType> addItemType(@Header("Authorization: Bearer ") String accessToken,
+                               @Query("active") Boolean active,
+                               @Query("name") String name,
+                               @Query("quantity") int quantitiy);
+
+    @GET("/itemtype/all")
+    Call<List<ItemType>> getItemTypes(@Header("Authorization: Bearer ") String accessToken);
+
+    @PUT("/itemtype/update/{itemtypeid}")
+    Call<ItemType> updateItemType(@Header("Authorization: Bearer ") String accessToken,
+                                  @Path("itemtypeid") String id,
+                                  @Query("active") Boolean active,
+                                  @Query("name") String name,
+                                  @Query("quantity") int quantitiy);
+
+    @POST("/users/newuser")
+    Call<User> newUser(@Header("Authorization: Bearer ") String accessToken,)
 }
